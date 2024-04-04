2764db4a8d1ca037a81e15916160ab5fb5f78e9a
Ashish Kumar <52818192+ashish13377@users.noreply.github.com>
2024-04-04 08:39:17 +0530

    Revert "Update app.js"

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const Wishlist = require('./models/wishlist');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Connect to MongoDB Ashish here
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
